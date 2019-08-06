d7cee34457fecb5fb3428c4f7e83ab7485ddcfde
Cyuhsuan <x89301089@gmail.com>
2019-08-06 18:13:20 +0800

    [add] modal

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,22 @@
 <template>
-  <header>
-    <div class="title">yuHsuanC</div>
-    <div class="group">
-      <div class="btn"></div>
-      <div class="btn"></div>
-      <div class="btn">menu</div>
+  <div>
+    <header>
+      <div class="title">yuHsuanC</div>
+      <div class="group">
+        <div class="btn"></div>
+        <div class="btn"></div>
+        <div class="btn">menu</div>
+      </div>
+    </header>
+    <div class="modal">
+      <div class="group_option">
+        <div class="option">home</div>
+        <div class="option">introduction</div>
+        <div class="option">tool</div>
+        <div class="option">plan</div>
+      </div>
     </div>
-  </header>
+  </div>
 </template>
 
 <style lang="scss">
@@ -25,6 +35,45 @@ header {
   }
   .btn {
     color: #fff;
+  }
+}
+
+.modal {
+  display: none;
+  // display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  width: 100%;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.7);
+}
+
+.group_option {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 400px;
+  height: 500px;
+  background: #222;
+  border: 2px solid #fff;
+  border-radius: 10px;
+
+  .option {
+    width: 70%;
+    height: 50px;
+    margin: 0 0 20px;
+    border-bottom: 1px solid #aaa; 
+    font-size: 30px;
+    text-align: center;
+    color: #aaa;
+
+    &:hover{
+      border-bottom: 1px solid #fff; 
+      color: #fff;
+    }
+
   }
 }
 </style>
