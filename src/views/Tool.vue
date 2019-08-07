e90845b621b593f4472b6bdcd3fd5eb1723cd3ad
Cyuhsuan <x89301089@gmail.com>
2019-08-07 15:19:44 +0800

    [add] icon

---
+++ src/views/Tool.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Banner :page="page" />
+    <Content :page="page" />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// import Header from "@/components/Header.vue";
+import Banner from "@/components/Banner.vue";
+import Content from "@/components/Content.vue";
+import Footer from "@/components/Footer.vue";
+export default {
+  name: "tool",
+  components: {
+    // Header: Header,
+    Banner: Banner,
+    Content: Content,
+    Footer: Footer
+  },
+  data() {
+    let page = "tool";
+    return { page };
+  }
+};
+</script>
